205a8d1cbe1461ccd35a500429399bb59876aa34
Hai Phan <pataha@users.noreply.github.com>
2018-08-22 11:35:11 +0700

    echo success msg

--- build_mir3g.sh
+++ build_mir3g.sh
@@ -12,50 +12,7 @@ cd openwrt-imagebuilder-ramips-mt7621.Linux-x86_64
 make image PROFILE=mir3g "PACKAGES=${RELEASE_MODULES}"
 
 if [ $? -eq 0 ]; then
-
-# 	echo "Begin upload the release: $RELEASE_NAME"
-
-# 	github-release release \
-# 		--user trinhpham \
-# 		--repo xiaomi-r3g-openwrt-builder \
-# 		--tag $RELEASE_NAME \
-# 		--name $RELEASE_NAME \
-# 		--description "CI build includes: ${RELEASE_MODULES}"
-		
-# 	github-release upload \
-# 		--user trinhpham \
-# 		--repo xiaomi-r3g-openwrt-builder \
-# 		--tag $RELEASE_NAME \
-# 		--name openwrt-ramips-mt7621-device-mir3g.manifest \
-# 		--file /src/openwrt-imagebuilder-ramips-mt7621.Linux-x86_64/bin/targets/ramips/mt7621/openwrt-ramips-mt7621-device-mir3g.manifest
-		
-# 	github-release upload \
-# 		--user trinhpham \
-# 		--repo xiaomi-r3g-openwrt-builder \
-# 		--tag $RELEASE_NAME \
-# 		--name openwrt-ramips-mt7621-mir3g-squashfs-rootfs0.bin \
-# 		--file /src/openwrt-imagebuilder-ramips-mt7621.Linux-x86_64/bin/targets/ramips/mt7621/openwrt-ramips-mt7621-mir3g-squashfs-rootfs0.bin
-		
-# 	github-release upload \
-# 		--user trinhpham \
-# 		--repo xiaomi-r3g-openwrt-builder \
-# 		--tag $RELEASE_NAME \
-# 		--name sha256sums \
-# 		--file /src/openwrt-imagebuilder-ramips-mt7621.Linux-x86_64/bin/targets/ramips/mt7621/sha256sums
-
-# 	github-release upload \
-# 		--user trinhpham \
-# 		--repo xiaomi-r3g-openwrt-builder \
-# 		--tag $RELEASE_NAME \
-# 		--name openwrt-ramips-mt7621-mir3g-squashfs-kernel1.bin \
-# 		--file /src/openwrt-imagebuilder-ramips-mt7621.Linux-x86_64/bin/targets/ramips/mt7621/openwrt-ramips-mt7621-mir3g-squashfs-kernel1.bin
-		
-# 	github-release upload \
-# 		--user trinhpham \
-# 		--repo xiaomi-r3g-openwrt-builder \
-# 		--tag $RELEASE_NAME \
-# 		--name openwrt-ramips-mt7621-mir3g-squashfs-sysupgrade.tar \
-# 		--file /src/openwrt-imagebuilder-ramips-mt7621.Linux-x86_64/bin/targets/ramips/mt7621/openwrt-ramips-mt7621-mir3g-squashfs-sysupgrade.tar
+	echo "Build has been complete successful"
 else
 	echo "Build has been failed"
 	exit 2
