7f64b8ec1dd1ab696b9301f8bb7eca5a8bae7235
Hai Phan <pataha@users.noreply.github.com>
2018-08-22 14:43:46 +0700

    update MTK driver branch

--- build_mir3g.sh
+++ build_mir3g.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 RELEASE_NAME=v0.1-$(date +%Y%m%d_%H%M%S)
-INCLUDED_PACKAGES="luci mt7603 uci2dat wificonf mtk-nvram mtk-luci-plugin"
+INCLUDED_PACKAGES="luci"
 EXCLUDED_PACKAGES="-wpad-mini -kmod-cfg80211 -kmod-mac80211 -kmod-mt76-core -kmod-mt7603 -kmod-mt76x2"
 
 echo "Begin build ${RELEASE_NAME} with modules ${RELEASE_MODULES}"
@@ -12,7 +12,7 @@ rm -f openwrt-imagebuilder-ramips-mt7621.Linux-x86_64.tar.xz
 cd openwrt-imagebuilder-ramips-mt7621.Linux-x86_64
 
 # add MTK driver
-echo "src-git mtk https://github.com/Nossiac/mtk-openwrt-feeds;master" >> feeds.conf.default
+echo "src-git mtk https://github.com/Nossiac/mtk-openwrt-feeds;lede-17.01" >> feeds.conf.default
 scripts/feeds update -f mtk
 scripts/feeds install -a -p mtk
 
