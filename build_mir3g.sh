09f3ed75a1dfd292b73d9e5bce60542d4cb4a423
Hai Phan <pataha@users.noreply.github.com>
2018-08-22 11:13:46 +0700

    remove unnecessary stuff

--- build_mir3g.sh
+++ build_mir3g.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 RELEASE_NAME=v0.1-$(date +%Y%m%d_%H%M%S)
-RELEASE_MODULES="luci minidlna luci-app-minidlna \
-	samba36-server transmission-web openvpn-openssl openssl-util \
-	luci-app-openvpn kmod-usb-storage kmod-fs-ext4 kmod-fs-vfat \
-	kmod-nls-cp437 kmod-nls-iso8859-1 kmod-fs-msdos kmod-fs-ntfs \
-	block-mount"
+RELEASE_MODULES="luci"
 
 echo "Begin build ${RELEASE_NAME} with modules ${RELEASE_MODULES}"
 	
@@ -17,49 +13,49 @@ make image PROFILE=mir3g "PACKAGES=${RELEASE_MODULES}"
 
 if [ $? -eq 0 ]; then
 
-	echo "Begin upload the release: $RELEASE_NAME"
+# 	echo "Begin upload the release: $RELEASE_NAME"
 
-	github-release release \
-		--user trinhpham \
-		--repo xiaomi-r3g-openwrt-builder \
-		--tag $RELEASE_NAME \
-		--name $RELEASE_NAME \
-		--description "CI build includes: ${RELEASE_MODULES}"
+# 	github-release release \
+# 		--user trinhpham \
+# 		--repo xiaomi-r3g-openwrt-builder \
+# 		--tag $RELEASE_NAME \
+# 		--name $RELEASE_NAME \
+# 		--description "CI build includes: ${RELEASE_MODULES}"
 		
-	github-release upload \
-		--user trinhpham \
-		--repo xiaomi-r3g-openwrt-builder \
-		--tag $RELEASE_NAME \
-		--name openwrt-ramips-mt7621-device-mir3g.manifest \
-		--file /src/openwrt-imagebuilder-ramips-mt7621.Linux-x86_64/bin/targets/ramips/mt7621/openwrt-ramips-mt7621-device-mir3g.manifest
+# 	github-release upload \
+# 		--user trinhpham \
+# 		--repo xiaomi-r3g-openwrt-builder \
+# 		--tag $RELEASE_NAME \
+# 		--name openwrt-ramips-mt7621-device-mir3g.manifest \
+# 		--file /src/openwrt-imagebuilder-ramips-mt7621.Linux-x86_64/bin/targets/ramips/mt7621/openwrt-ramips-mt7621-device-mir3g.manifest
 		
-	github-release upload \
-		--user trinhpham \
-		--repo xiaomi-r3g-openwrt-builder \
-		--tag $RELEASE_NAME \
-		--name openwrt-ramips-mt7621-mir3g-squashfs-rootfs0.bin \
-		--file /src/openwrt-imagebuilder-ramips-mt7621.Linux-x86_64/bin/targets/ramips/mt7621/openwrt-ramips-mt7621-mir3g-squashfs-rootfs0.bin
+# 	github-release upload \
+# 		--user trinhpham \
+# 		--repo xiaomi-r3g-openwrt-builder \
+# 		--tag $RELEASE_NAME \
+# 		--name openwrt-ramips-mt7621-mir3g-squashfs-rootfs0.bin \
+# 		--file /src/openwrt-imagebuilder-ramips-mt7621.Linux-x86_64/bin/targets/ramips/mt7621/openwrt-ramips-mt7621-mir3g-squashfs-rootfs0.bin
 		
-	github-release upload \
-		--user trinhpham \
-		--repo xiaomi-r3g-openwrt-builder \
-		--tag $RELEASE_NAME \
-		--name sha256sums \
-		--file /src/openwrt-imagebuilder-ramips-mt7621.Linux-x86_64/bin/targets/ramips/mt7621/sha256sums
+# 	github-release upload \
+# 		--user trinhpham \
+# 		--repo xiaomi-r3g-openwrt-builder \
+# 		--tag $RELEASE_NAME \
+# 		--name sha256sums \
+# 		--file /src/openwrt-imagebuilder-ramips-mt7621.Linux-x86_64/bin/targets/ramips/mt7621/sha256sums
 
-	github-release upload \
-		--user trinhpham \
-		--repo xiaomi-r3g-openwrt-builder \
-		--tag $RELEASE_NAME \
-		--name openwrt-ramips-mt7621-mir3g-squashfs-kernel1.bin \
-		--file /src/openwrt-imagebuilder-ramips-mt7621.Linux-x86_64/bin/targets/ramips/mt7621/openwrt-ramips-mt7621-mir3g-squashfs-kernel1.bin
+# 	github-release upload \
+# 		--user trinhpham \
+# 		--repo xiaomi-r3g-openwrt-builder \
+# 		--tag $RELEASE_NAME \
+# 		--name openwrt-ramips-mt7621-mir3g-squashfs-kernel1.bin \
+# 		--file /src/openwrt-imagebuilder-ramips-mt7621.Linux-x86_64/bin/targets/ramips/mt7621/openwrt-ramips-mt7621-mir3g-squashfs-kernel1.bin
 		
-	github-release upload \
-		--user trinhpham \
-		--repo xiaomi-r3g-openwrt-builder \
-		--tag $RELEASE_NAME \
-		--name openwrt-ramips-mt7621-mir3g-squashfs-sysupgrade.tar \
-		--file /src/openwrt-imagebuilder-ramips-mt7621.Linux-x86_64/bin/targets/ramips/mt7621/openwrt-ramips-mt7621-mir3g-squashfs-sysupgrade.tar
+# 	github-release upload \
+# 		--user trinhpham \
+# 		--repo xiaomi-r3g-openwrt-builder \
+# 		--tag $RELEASE_NAME \
+# 		--name openwrt-ramips-mt7621-mir3g-squashfs-sysupgrade.tar \
+# 		--file /src/openwrt-imagebuilder-ramips-mt7621.Linux-x86_64/bin/targets/ramips/mt7621/openwrt-ramips-mt7621-mir3g-squashfs-sysupgrade.tar
 else
 	echo "Build has been failed"
 	exit 2
